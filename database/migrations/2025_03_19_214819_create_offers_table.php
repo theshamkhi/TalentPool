337b930636258a088387fa2c461cf6962fd5d2dc
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-19 22:28:07 +0000

    Added necessary migration tables

---
+++ database/migrations/2025_03_19_214819_create_offers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->text('requirements');
+            $table->foreignId('recruiter_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
