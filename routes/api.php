40ee63a59329dd516ed38023dbcfe476fc8c0700
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-20 15:33:29 +0000

    Added Applications routes & tested them with Postman

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OfferController;
+use App\Http\Controllers\AppController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -11,3 +12,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::apiResource('offers', OfferController::class);
+
+
+Route::post('/offer/{offerID}/apply', [AppController::class, 'apply'])->middleware('auth:sanctum');
+
+Route::delete('/applications/{appID}/withdraw', [AppController::class, 'withdraw'])->middleware('auth:sanctum');
+
+Route::get('/recruiter/applications', [AppController::class, 'getApps'])->middleware('auth:sanctum');
