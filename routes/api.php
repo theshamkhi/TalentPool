dc2f1e1bfea8ee3eecb2c8d559622253acc8ac97
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-20 12:45:54 +0000

    Added Offers apiResource route

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OfferController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::apiResource('offers', OfferController::class);
