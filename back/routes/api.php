e1d2962c43160d5e998b1d0a12f70b7582bf58fc
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-04-10 10:04:11 +0100

    Added route for /user

--- back/routes/api.php
+++ back/routes/api.php
@@ -9,8 +9,11 @@ use App\Http\Controllers\AppController;
 // A U T H
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/forgot-password', [AuthController::class, 'sendReset']);
-Route::post('/reset-password', [AuthController::class, 'passReset']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+// Route::post('/forgot-password', [AuthController::class, 'sendReset']);
+// Route::post('/reset-password', [AuthController::class, 'passReset']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
