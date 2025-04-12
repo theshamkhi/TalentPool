7555b9cfd5a1c7976ba8e67da0f1b97141210ae1
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-04-12 18:14:09 +0100

    Updated routes to align with the frontend

--- back/routes/api.php
+++ back/routes/api.php
@@ -6,24 +6,25 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OfferController;
 use App\Http\Controllers\AppController;
 
-// A U T H
-Route::post('/register', [AuthController::class, 'register']);
+
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-// Route::post('/forgot-password', [AuthController::class, 'sendReset']);
-// Route::post('/reset-password', [AuthController::class, 'passReset']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
     Route::post('/logout', [AuthController::class, 'logout']);
-// O F F E R S
-    Route::apiResource('offers', OfferController::class);
-// A P P L Y  &  W I T H D R A W
-    Route::post('/offer/{offerID}/apply', [AppController::class, 'apply']);
-    Route::delete('/applications/{appID}/withdraw', [AppController::class, 'withdraw']);
-// R E C R U I T E R
-    Route::get('/recruiter/applications', [AppController::class, 'getApps']);
+
+    Route::apiResource('offers', OfferController::class)->except(['create', 'edit']);
+
+    Route::post('/offers/{offer}/apply', [AppController::class, 'apply']);
+    
+    Route::delete('/applications/{app}/withdraw', [AppController::class, 'withdraw']);
+    Route::put('/applications/{app}/updateStatus', [AppController::class, 'updateAppStatus']);
+
+    Route::get('/candidate/applications', [AppController::class, 'getCandidateApps']);
+
+    Route::get('/recruiter/applications', [AppController::class, 'getRecruiterApps']);
     Route::get('/recruiter/statistics', [AppController::class, 'getRecruiterStats']);
-    Route::put('/applications/{appID}/updateStatus', [AppController::class, 'updateAppStatus']);
 });
